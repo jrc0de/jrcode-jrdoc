055267b969ba0e517eee2caea093cdd0d9752321
JacquesRaoult <raoult.jacques@gmail.com>
2026-07-13 16:39:59 +0200

    Remove footer border

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -7,6 +7,9 @@ export default defineConfig({
   description: "Web dev doc",
   appearance: "force-dark",
   themeConfig: {
+    footer: {
+      copyright: "© 2026 JRCode",
+    },
     socialLinks: [{ icon: "github", link: "https://github.com/jrc0de" }],
     nav: [
       {
--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,5 +1,6 @@
 import DefaultTheme from "vitepress/theme"
 import MeiViewer from "./MeiViewer.vue"
+import "./custom.css"
 
 export default {
   extends: DefaultTheme,
