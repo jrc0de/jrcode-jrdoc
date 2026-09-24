4d598422c275006e52d1b376b60fd1b4f7d23756
jrc0de <raoult.jacques@gmail.com>
2026-09-24 20:25:33 +0200

    Add update date support

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -7,6 +7,7 @@ export default withMermaid(
     title: "JRDoc",
     description: "Web dev doc",
     appearance: "force-dark",
+    lastUpdated: true,
     vite: {
       optimizeDeps: {
         include: ["mermaid", "fastdom"],
@@ -16,6 +17,12 @@ export default withMermaid(
       logo: "/img/rocket.png",
       footer: {
         copyright: "© 2026 ⚡ JRCode",
+      },
+      lastUpdated: {
+        text: "Mis à jour le",
+        formatOptions: {
+          dateStyle: "short",
+        },
       },
       nav: [
         {
